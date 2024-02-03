f06d0be22091096949d248e829fc9440873b41fc
yourhwan <yunhwan9508@gmail.com>
2024-02-03 20:45:05 +0900

    새로운 프로젝트 클래스 초안(수정 필요)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        // 모든 캐릭터를 나타내는 기본 클래스
         class Character {
             String name; // 이름
             int level; // 레벨
