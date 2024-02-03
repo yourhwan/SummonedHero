9f18708f376a1f9180a48c2753cba50a93ed3c3c
yourhwan <yunhwan9508@gmail.com>
2024-02-03 22:34:41 +0900

    무기관련 코드 설계구성 예시(까먹지 않기 위해 작성)

---
+++ src/Weapon.java
@@ -0,0 +1,29 @@
+public class Weapon implements Fight {
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public int getDamage() {
+        return 10;
+    }
+
+}
+
+class LongSword extends Weapon {
+
+    public LongSword() {
+
+    }
+
+}
+
+class ShortSword extends Weapon {
+
+    public LongSword() {
+
+    }
+
+}
