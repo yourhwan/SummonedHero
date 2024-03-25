0e7e6e690581e0105fa80064032d3345db7bca72
yourhwan <yunhwan9508@gmail.com>
2024-03-25 22:05:04 +0900

    -공격스킬 및 버프스킬 인터페이스 메서드 반환유형 수정 -버서커, 워리어, 히어로 클래스 기본 구성 완료, 추후 수정 또는 추가 필요 -듀얼블레이드 작성 시작
    *****
    모든 직업클래스 확인 필요(특히 메서드 안에 있는 if문에서 hp와 mp 설정 부분) -> set으로 직접 변경해주는 것이 아닌 지역변수 선언 후 변경하도록 코드 재구성해야함.
    
    **************
    전투 중 레벨업을 했을 경우 걸려있던 버프 해제 후 레벨업 스탯 적용 후 다시 버프 적용되도록 수정 필요
    
    -to do 목록-
    1.남은 직업 모두 구현
    2.스킬 인터페이스 구현(직업별 공격 및 버프, 우선 1차까지)
    3.몬스터 사냥시 경험치 획득 자동화 메서드 구현
    4.슈퍼클래스인 Monster 클래스 구현
    5.Monster 클래스의 하위 클래스 구현
    6.메인스토리에서 플레이 스토리 구현
    7.전투관련 클래스 또는 메서드 구현(전투시 패시브 메서드 자동 실행 및 전투 종료시 자동 해제, 경험치 누적 및 레벨업, 전투 종료시 버프 효과 해제)

--- src/userjob/DualBlade.java
+++ src/userjob/DualBlade.java
@@ -15,12 +15,12 @@ public class DualBlade extends Hero implements DualBladeAttackSkill {
         setMoney(getMoney());           // 돈
         setJob("듀얼블레이드");            // 직업
         setBasicAttackName("");         // 기본 공격 이름
-        setBasicAttackDamage(40);       // 기본 공격 데미지
+        setBasicAttackDamage(30);       // 기본 공격 데미지
         setInitialMaxHp(150);           // 버프 스킬 사용 전 최대 체력
         setInitialHp(150);              // 버프 스킬 사용 전 체력
         setInitialMaxMp(150);           // 버프 스킬 사용 전 최대 마력
         setInitialMp(150);              // 버프 스킬 사용 전 마력
-        setInitialDamage(40);           // 버프 스킬 사용 전 기본 공격력
+        setInitialDamage(30);           // 버프 스킬 사용 전 기본 공격력
     }
 
     // 듀얼블레이드 기본 공격
@@ -30,10 +30,10 @@ public class DualBlade extends Hero implements DualBladeAttackSkill {
         int baseDamage = getBasicAttackDamage(); // 듀얼블레이드에게 부여한 기본 데미지
 
         boolean isRandom = Math.random() <= 0.8; // 랜덤 데미지 추가 확률 설정
-        int randomDamage = isRandom ? (int) (Math.random() * 7.0) + 1 : 0; // 랜덤 데미지 부여
+        int randomDamage = isRandom ? (int) (Math.random() * 7) + 1 : 0; // 랜덤 데미지 부여
 
         boolean isCritical = Math.random() <= 0.8; // 크리티컬 추가 데미지 확률 설정
-        int criticalDamage = isCritical ? 30 : 10; // 크리티컬일 경우 40의 데미지, 아닐 경우 0
+        int criticalDamage = isCritical ? 50 : 10; // 크리티컬일 경우 50의 데미지, 아닐 경우 0
 
         int totalDamage = baseDamage + randomDamage + criticalDamage; // 기본 데미지와 랜덤 데미지 합치기
 
